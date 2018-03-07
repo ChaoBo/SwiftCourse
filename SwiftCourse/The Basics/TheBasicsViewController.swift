8bbf71252e59c1d5f07b1fc9889c4b1df77180fc
晁博 <631616788@qq.com>
2018-03-08 00:52:31 +0800

    [Commit Message]新增项目

---
+++ SwiftCourse/The Basics/TheBasicsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TheBasicsViewController.swift
+//  SwiftCourse
+//
+//  Created by 晁博 on 2018/3/8.
+//  Copyright © 2018年 SwiftCourse. All rights reserved.
+//
+
+import UIKit
+
+class TheBasicsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "The Basics"
+        
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
